2421c9b1fee797f71b176bc35030018be1faafbf
rajali <rajali1432@gmail.com>
2024-03-04 22:32:11 +0530

    make feed component , change in app.js and home.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import {
-  Routes,Route
-  
+  Routes, Route
+
 } from "react-router-dom";
 import ProductDetails from './pages/ProductDetails';
 import Categories from './pages/Categories';
@@ -18,32 +16,44 @@ import OrderList from './components/Admin/OrderList';
 import ProductList from './components/Admin/ProductList';
 import AddProduct from './components/Admin/AddProduct';
 import Orders from './pages/Orders';
+import Feed from './components/Feed';
 
 function App() {
-  return (
-    <div className='bg-white' >
-         <Navbar/>
-         <Routes>
-            <Route path='/' element={<Home/>}></Route>
-            <Route path='/profile/:userId' element={<Profile/>}></Route>
-            <Route path='/product-details/:productId' element={<ProductDetails/>}></Route>
-            <Route path='/categories/:category?' element={<Categories/>}></Route>
-            <Route path='/payment/:status' element={<PaymentStatus/>}></Route>
-            <Route path='/orders' element={<Orders/>}></Route>
-            <Route path='/admin' element={<AdminScreen/>}>
-               <Route path='' element={<UserList/>}></Route>
-               <Route path='userlist' element={<UserList/>}></Route>
-               <Route path='all-products' element={<ProductList/>}></Route>
-               <Route path='add-product' element={<AddProduct/>}></Route>
-               <Route path='orderlist' element={<OrderList/>}></Route>
-            </Route>
 
-            <Route path='/login' element={<Login/>}></Route>
-            <Route path='/signup' element={<Signup/>}></Route>
-         </Routes>
-         
-         <Footer/>
-    </div>
+
+
+
+  
+  return (
+    <>
+
+      <Routes>
+        <Route >
+          <Route element={<Home />}>
+            <Route path='/' element={<Feed />}></Route>
+            <Route path='/profile/:userId' element={<Profile />}></Route>
+            <Route path='/product-details/:productId' element={<ProductDetails />}></Route>
+            <Route path='/categories/:category?' element={<Categories />}></Route>
+            <Route path='/payment/:status' element={<PaymentStatus />}></Route>
+            <Route path='/orders' element={<Orders />}></Route>
+            <Route path='/admin' element={<AdminScreen />}>
+              <Route path='' element={<UserList />}></Route>
+              <Route path='userlist' element={<UserList />}></Route>
+              <Route path='all-products' element={<ProductList />}></Route>
+              <Route path='add-product' element={<AddProduct />}></Route>
+              <Route path='orderlist' element={<OrderList />}></Route>
+            </Route>
+          </Route>
+        </Route>
+
+        <Route >
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/signup' element={<Signup />}></Route>
+        </Route>
+      </Routes>
+
+    </>
+
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,55 +1,18 @@
 import React from 'react'
-import Hero from '../components/Hero'
-import Collections from '../components/Collections'
-import Product from '../components/Product'
-import Features from '../components/Features'
+import Navbar from '../components/Navbar'
+import { Outlet } from 'react-router-dom'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <>
-      <Hero />
-      <div className='px-14 py-10 '>
-        <div className='flex flex-col flex-wrap'>
-          <div className='text-center mb-12'>
-            <p className='sm:text-4xl text-3xl font-bold uppercase tracking-wide '>Collections</p>
-          </div>
-          <div className='flex flex-wrap md:gap-4 gap-2 justify-center'>
-            <Collections />
-            <Collections />
-            <Collections />
-            <Collections />
-           
-
-          </div>
-
-
-        </div>
+      
+      <Navbar/>
+      <div className='outlet'>
+           <Outlet/>
       </div>
-
-      <div className='px-14 py-10' >
-
-        <div className='flex flex-col gap-y-6'>
-          <div>
-            <p className='sm:text-4xl text-3xl ms-6  text-black font-semibold tracking-wide'>Bestselling Products</p>
-            <div className=' ms-6 h-2  border-b-4 rounded-md border-red-400 w-32 mt-2'></div>
-          </div>
-          <div className='flex flex-wrap gap-2 md:gap-4 justify-center'>
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-          </div>
-        </div>
-      </div>
-
-       
-       <Features/>
-     
-     
-     {/* Footer start here */}
+      <Footer/>
     
-
 
     </>
   )
